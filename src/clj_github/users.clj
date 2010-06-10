549b5878aa738509a592a8c2893be308f099bfab
Raynes <DiscipleRayne@gmail.com>
2010-06-10 09:21:53 -0500

    Killed off the "User" section of the API docs. All sorts of new stuff. This is so much fun.

--- src/clj_github/users.clj
+++ src/clj_github/users.clj
@@ -17,3 +17,28 @@
   [user target value]
   {:pre [(#{"name" "email" "blog" "company" "location"} target)]}
   (handle (make-request (str "/user/show/" user) :type "POST" :data {(str "values[" target "]") value})))
+
+(defn following
+  "Get a list of the users that a user is following."
+  [user]
+  (handle (make-request (str "/user/show/" user "/following")) :users))
+
+(defn followers
+  "Get a list of users that are following a user."
+  [user]
+  (handle (make-request (str "/user/show/" user "/followers")) :users))
+
+(defn follow
+  "Follow a user."
+  [user]
+  (handle (make-request (str "/user/follow/" user) :type "POST")))
+
+(defn unfollow
+  "Unfollow a user."
+  [user]
+  (handle (make-request (str "/user/unfollow/" user) :type "POST")))
+
+(defn watching
+  "Get a list of repos that user is watching."
+  [user]
+  (handle (make-request (str "/repos/watched/" user)) :repositories))
