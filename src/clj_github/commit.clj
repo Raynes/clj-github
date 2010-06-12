5092f0001722bc32d8d7ffc3f7adcf47022db941
Raynes <DiscipleRayne@gmail.com>
2010-06-12 08:38:36 -0500

    Finished commits API.

--- src/clj_github/commit.clj
+++ src/clj_github/commit.clj
@@ -3,6 +3,13 @@
   (:use [clj-github.core]))
 
 (defn list-commits
-  "Get the latest commits in a branch of a repository."
-  [user repo branch]
-  (handle (make-request ["commits/list" user repo branch]) :commits))
+  "Get the latest commits in a branch of a repository. If path is supplied, the path
+  will be considered a path to a file, and list-commits will return the commits for
+  that file. There should be no trailing slashes in the path."
+  [user repo branch & [path]]
+  (handle (make-request ["commits/list" user repo branch path]) :commits))
+
+(defn show-commit
+  "Show data about a specific commit."
+  [user repo sha]
+  (handle (make-request ["commits/show" user repo sha]) :commit))
