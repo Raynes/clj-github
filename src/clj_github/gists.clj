28025f32173f42411dc9935c9f4a244d0ca6ebdf
Raynes <DiscipleRayne@gmail.com>
2010-06-15 04:09:48 -0500

    Completed all of the gist stuff that I know how to do, including a function for posting new gists. This isn't even documented. I'll add more stuff as more stuff gets documented.

---
+++ src/clj_github/gists.clj
@@ -0,0 +1,29 @@
+(ns #^{:doc "Implements the API functions described here: http://develop.github.com/p/gist.html"}
+  clj-github.gist
+  (:use clj-github.core))
+
+(defn show-gist-meta
+  "Get a gist's metadata."
+  [id]
+  (make-gist-request (str id) :sift :gists))
+
+(defn show-gist
+  "Get the contents of a gist."
+  [id file-name]
+  (make-gist-request [id file-name] :special "/raw/" :raw? true))
+
+(defn show-users-gists
+  "Show metadata for gists by another user."
+  [user]
+  (make-gist-request ["gists" user] :sift :gists))
+
+(defn new-gist
+  "Create a new gist. You can supply as many filename -> contents pairs as you like.
+  Syntax highlighting is determined by extension."
+  [& files]
+  (make-gist-request "new"
+                     :type "POST"
+                     :data (into {}
+                                 (for [[k v] (partition 2 files)]
+                                   [(str "files[" k "]") v]))
+                     :sift :gists))
