9337f109f848e051d0f37db68bfb5959b43f844b
Raynes <DiscipleRayne@gmail.com>
2010-06-15 05:22:27 -0500

    Finished the shitty network API.

---
+++ src/clj_github/network.clj
@@ -0,0 +1,15 @@
+(ns #^{:doc "Implements the API functions described here: http://develop.github.com/p/network.html"}
+  clj-github.network
+  (:use clj-github.core))
+
+(defn show-network-meta
+  "Show network metadata for a repo."
+  [user repo]
+  (make-request [user repo "network_meta"] :special "/"))
+
+(defn show-network-data
+  "Show network data for a repo. You'll need a nethash from a show-network-meta call."
+  [user repo nethash]
+  (make-request [user repo "network_data_chunk"]
+                :special "/"
+                :data {"nethash" nethash}))
