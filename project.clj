9ba9273fc2981a93417bb023b35d6993354a4f2d
Raynes <DiscipleRayne@gmail.com>
2010-07-19 17:46:53 -0500

    Updated to Clojure 1.2-beta1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-github "1.0.0-SNAPSHOT"
   :description "Bindings to the Github and Gist API."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [clojure-http-client "1.1.0-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+		 [org.clojars.rayne/clojure-http-client "1.1.0-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
 		 [org.danlarkin/clojure-json "1.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [autodoc "0.7.0"]])
