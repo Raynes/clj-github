0903a2c6b6cf691d3603a1ca8744d85bef432ca2
Raynes <DiscipleRayne@gmail.com>
2010-09-21 22:15:58 -0500

    Added cake-autodoc.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
 		 [clj-http "0.1.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [cake-autodoc "0.0.1-SNAPSHOT"]]
+  :tasks [cake-autodoc.tasks])
