a6ee5109db0e3d7b9fa81f8f3e90c6a628517232
Paul deGrandis <paul.degrandis@gmail.com>
2010-12-15 12:24:02 -0800

    Updated project dependencies and ignore file to be more comprehensive

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-github "1.0.0-SNAPSHOT"
   :description "Bindings to the Github and Gist API."
   :dependencies [[org.clojure/clojure "1.2.0"]
-		 [clj-http "0.1.1"]
+		         [clj-http "0.1.2"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [cake-autodoc "0.0.1-SNAPSHOT"]]
