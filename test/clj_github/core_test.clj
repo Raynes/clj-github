49a520f64494cbb23bdba7ceb7599ffbbabe02de
Paul deGrandis <paul.degrandis@gmail.com>
2010-12-16 08:59:51 -0800

    tests for slash-join in core

--- test/clj_github/core_test.clj
+++ test/clj_github/core_test.clj
@@ -2,5 +2,19 @@
   (:use [clj-github.core] :reload-all)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(deftest test-slash-join
+  (let [str-case ["a" "b" "c"]
+        expected-str-case "a/b/c"
+        num-case ["a" 2 "c"]
+        expected-num-case "a/2/c"
+        vec-case ["a" [1] "c"]
+        expected-vec-case "a/[1]/c"]
+    (is (= expected-str-case (apply slash-join str-case)))
+    (is (= expected-num-case (apply slash-join num-case)))
+    (is (= expected-vec-case (apply slash-join vec-case)))
+    (is (= "" (apply slash-join [])))
+    (is (= "" (slash-join nil)))
+    (is (= "" (slash-join)))))
+
+
+
