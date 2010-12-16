05e63562857c000393bea5be44312e8b8128088b
Paul deGrandis <paul.degrandis@gmail.com>
2010-12-16 09:00:43 -0800

    Bumped the version because the API names changed a little

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-github "1.0.0-SNAPSHOT"
+(defproject clj-github "1.0.1-SNAPSHOT"
   :description "Bindings to the Github and Gist API."
   :dependencies [[org.clojure/clojure "1.2.0"]
 		         [clj-http "0.1.2"]
