ffb030b648dfea65f2d9d1ea068c66c7af395bed
Raynes <i@raynes.me>
2011-04-22 16:28:10 -0500

    Real version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-github "1.0.1-SNAPSHOT"
+(defproject clj-github "1.0.1"
   :description "Bindings to the Github and Gist API."
   :dependencies [[org.clojure/clojure "1.2.0"]
 		         [clj-http "0.1.2"]
